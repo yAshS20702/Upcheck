6d364e2b369c380e94a0324a54422e5398e9cab5
yash <yashraj20702@gmail.com>
2023-02-20 11:37:26 +0530

    Initial Commit

---
+++ Upcheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Upcheck
+//
+//  Created by Thinkwik 27 on 20/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
